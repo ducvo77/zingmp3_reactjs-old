6c714de29bbdb775ff35e65177d168f80dce3acf
Coder1603 <congductp1@gmail.com>
2023-03-03 00:29:52 +0700

    Get API + 10% Home Page

---
+++ src/untils/homeGallery.js
@@ -0,0 +1,16 @@
+const GALLERY = [
+  {
+    alt: "",
+    src: "https://photo-zmp3.zmdcdn.me/banner/4/9/9/8/49987074fe79ba7736517a688e6fb0bc.jpg",
+  },
+  {
+    alt: "",
+    src: "https://photo-zmp3.zmdcdn.me/banner/4/9/9/8/49987074fe79ba7736517a688e6fb0bc.jpg",
+  },
+  {
+    alt: "",
+    src: "https://photo-zmp3.zmdcdn.me/banner/4/9/9/8/49987074fe79ba7736517a688e6fb0bc.jpg",
+  },
+];
+
+export default GALLERY;
